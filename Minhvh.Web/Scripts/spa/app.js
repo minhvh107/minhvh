0df70cb34091bd174f73dff9dc036383e5d6cb73
Minhvh <minhvh107@gmail.com>
2017-09-07 19:03:02 +0700

    Update

--- Minhvh.Web/Scripts/spa/app.js
+++ Minhvh.Web/Scripts/spa/app.js
@@ -1,9 +1,26 @@
-﻿
-
-var myApp = angular.module('myModule', []);
+﻿var myApp = angular.module('myModule', []);
 
 myApp.controller("schoolController", schoolController);
+myApp.service("validator", validator);
 
-function schoolController($scope) {
-    $scope.message = "Ahihi";
+schoolController.$inject =['$scope', 'validator'];
+
+function schoolController($scope, validator) {
+    validator.checkNumber(1);
+    //$scope.message = "Ahihi";
+}
+
+
+function validator($window) {
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            $window.alert("day la so chan");
+        } else {
+            $window.alert("day la so le");
+        }
+    }
+
+    return {
+        checkNumber: checkNumber
+    }
 }
